aea165a44d2aea71def86364a9835567426ad17b
Kim jung won <k38836919@gmail.com>
2023-03-27 01:40:40 +0900

    refact : SignUp Feature

--- src/main/java/com/capstone/project/controller/MemberController.java
+++ src/main/java/com/capstone/project/controller/MemberController.java
@@ -1,2 +1,45 @@
-package com.capstone.project.controller;public class MemberController {
+package com.capstone.project.controller;
+
+import com.capstone.project.domain.dto.MemberDto;
+import com.capstone.project.domain.dto.SignUpDto;
+import com.capstone.project.service.MemberService;
+import com.capstone.project.utils.TokenUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Log4j2
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+    private TokenUtils tokenUtils;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<MemberDto> signUp(@RequestBody SignUpDto signUpDto, HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        log.info("사용자 이름 : {}", signUpDto.getName());
+        log.info("사용자 이메일 : {}", signUpDto.getEmail());
+        log.info("사용자 비밀번호 : {}", signUpDto.getPassword());
+
+        if(memberService.isEmailDuplicated(signUpDto.getEmail())){
+            log.error("이미 존재하는 회원 이메일입니다.");
+            return ResponseEntity.badRequest().build();
+        } else{
+            MemberDto memberDto = memberService.signUp(signUpDto);
+            ResponseEntity.status(200);
+            return ResponseEntity.ok(memberDto);
+        }
+    }
+
 }
--- src/main/java/com/capstone/project/service/MemberService.java
+++ src/main/java/com/capstone/project/service/MemberService.java
@@ -1,2 +1,48 @@
-package com.capstone.project.service;public class MemberService {
+package com.capstone.project.service;
+
+import com.capstone.project.domain.dto.MemberDto;
+import com.capstone.project.domain.dto.SignUpDto;
+import com.capstone.project.domain.entity.Member;
+import com.capstone.project.domain.enums.UserRole;
+import com.capstone.project.repository.MemberRepository;
+import com.capstone.project.utils.TokenUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final TokenUtils tokenUtils;
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Boolean isEmailDuplicated(final String email){
+        return memberRepository.existsByEmail(email);
+    }
+
+    public Member findByEmail(String email){
+        return memberRepository.findByEmail(email).orElse(null);
+    }
+
+    @Transactional
+    public MemberDto signUp(final SignUpDto signUpDto){
+        memberRepository.save(
+                Member.builder()
+                        .name(signUpDto.getName())
+                        .email(signUpDto.getEmail())
+                        .password(bCryptPasswordEncoder.encode(signUpDto.getPassword()))
+                        .role(UserRole.USER)
+                        .build());
+
+        return MemberDto.builder()
+                .name(signUpDto.getName())
+                .email(signUpDto.getEmail())
+                .build();
+    }
 }
