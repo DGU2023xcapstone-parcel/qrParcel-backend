902cc54bf9bb58d9eb99d08215e570cdbc456a39
Kim jung won <k38836919@gmail.com>
2023-03-26 22:42:15 +0900

    feat : Create Member Entity

--- src/main/java/com/capstone/project/domain/entity/Member.java
+++ src/main/java/com/capstone/project/domain/entity/Member.java
@@ -1,2 +1,38 @@
-package com.capstone.project.domain.entity;public class Member {
+package com.capstone.project.domain.entity;
+
+import com.capstone.project.BaseTimeEntity;
+import com.capstone.project.domain.enums.UserRole;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseTimeEntity implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
 }
