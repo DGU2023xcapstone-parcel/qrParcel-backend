ce4b50bc71b152f56727fd02794c6ede7faa5b16
Kim jung won <k38836919@gmail.com>
2023-03-30 18:37:47 +0900

    refact : swagger config

--- src/main/java/com/capstone/project/config/swagger/SwaggerConfig.java
+++ src/main/java/com/capstone/project/config/swagger/SwaggerConfig.java
@@ -1,2 +1,34 @@
-package com.capstone.project.config.swagger;public class SwaggerConfig {
+package com.capstone.project.config.swagger;
+
+import io.swagger.annotations.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())   // 현재 RequestMapping으로 할당된 모든 URL 리스트 추출
+                .paths(PathSelectors.any())            // PathSelectorys.any("/api/**")) 와 같이 /api/** 인 URL로만 필터링 할 수 있습니다.
+                .build();
+    }
+
 }
