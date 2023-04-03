b96b1408af50915a7b2515d3c19df159fb07784e
Kim jung won <k38836919@gmail.com>
2023-04-03 16:22:42 +0900

    removed cached

--- src/main/java/com/capstone/project/config/web/WebMvcConfig.java
+++ src/main/java/com/capstone/project/config/web/WebMvcConfig.java
@@ -1,2 +1,54 @@
-package com.capstone.project.config.web;public class WebMvcConfig {
+package com.capstone.project.config.web;
+
+import com.capstone.project.login.filter.HeaderFilter;
+import com.capstone.project.login.interceptor.JwtTokenInterceptor;
+import com.capstone.project.utils.TokenUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final TokenUtils tokenUtils;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor())
+                .excludePathPatterns("/member/signUp", "/member/signIn", "/api/**")
+                .addPathPatterns("/member/**", "/chat/**", "/url/**");
+    }
+
+
+    @Bean
+    public FilterRegistrationBean<HeaderFilter> getFilterRegistrationBean(){
+        FilterRegistrationBean<HeaderFilter> registrationBean = new FilterRegistrationBean<>(createHeaderFilter());
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true)
+                .maxAge(3600)
+                .allowedOriginPatterns("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+
+    @Bean
+    public HeaderFilter createHeaderFilter(){
+        return new HeaderFilter();
+    }
+
+    @Bean
+    public JwtTokenInterceptor jwtTokenInterceptor(){
+        return new JwtTokenInterceptor(tokenUtils);
+    }
 }
--- src/main/java/com/capstone/project/controller/ApiController.java
+++
@@ -1,2 +0,0 @@
-package com.capstone.project.controller;public class ApiController {
-}
