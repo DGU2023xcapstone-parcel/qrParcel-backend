41bc30a11dabe194e376c780e4c5b7cbeda24150
Kim jung won <k38836919@gmail.com>
2023-04-03 13:00:44 +0900

    refact : DTO Swagger 적용

--- src/main/java/com/capstone/project/domain/dto/SignUpDto.java
+++ src/main/java/com/capstone/project/domain/dto/SignUpDto.java
@@ -1,12 +1,18 @@
 package com.capstone.project.domain.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class SignUpDto {
 
+    @ApiModelProperty(value = "회원 이름", dataType = "string", required = true)
     private String name;
+
+    @ApiModelProperty(value = "회원 이메일")
     private String email;
+
+    @ApiModelProperty(value = "회원 비밀번")
     private String password;
 
 }
