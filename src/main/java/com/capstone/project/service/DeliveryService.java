3eae2df72f8987efc0bd925c1a8dca8fbcffe50d
Kim jung won <k38836919@gmail.com>
2023-04-16 14:21:18 +0900

    feat : 배송 기사 Service Layer 생성

--- src/main/java/com/capstone/project/service/DeliveryService.java
+++ src/main/java/com/capstone/project/service/DeliveryService.java
@@ -1,2 +1,14 @@
-package com.capstone.project.service;public class DeliveryService {
+package com.capstone.project.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class DeliveryService {
+
 }
