f6d767af9c96acf9315ee64515704054f295a2b7
Kim jung won <k38836919@gmail.com>
2023-04-20 13:01:47 +0900

    feat : 배송 물품 Repository 개발

--- src/main/java/com/capstone/project/repository/TrackingInfoRepository.java
+++ src/main/java/com/capstone/project/repository/TrackingInfoRepository.java
@@ -1,2 +1,11 @@
-package com.capstone.project.repository;public interface ParcelProductRepository {
+package com.capstone.project.repository;
+
+import com.capstone.project.domain.entity.TrackingInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TrackingInfoRepository extends JpaRepository<TrackingInfo, Long> {
+
+    Optional<TrackingInfo> findByInvoiceNo(String invoiceNo);
 }
