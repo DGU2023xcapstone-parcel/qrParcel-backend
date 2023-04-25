837b9ef1bcb7889231a1a600e1eb2617b12a5dba
Kim jung won <k38836919@gmail.com>
2023-04-25 15:14:50 +0900

    refact : HTTP 상태 메세지 출력 수정

--- src/main/java/com/capstone/project/config/swagger/SwaggerConfig.java
+++ src/main/java/com/capstone/project/config/swagger/SwaggerConfig.java
@@ -55,7 +55,12 @@ public class SwaggerConfig  {
                 .apis(RequestHandlerSelectors.basePackage("com.capstone.project.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .globalResponseMessage(RequestMethod.GET, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.POST, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.DELETE, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.PUT, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.PATCH, getCustomizedResponseMessages());
     }
 
     // 추가
@@ -125,17 +130,17 @@ public class SwaggerConfig  {
 //                .globalResponseMessage(RequestMethod.PATCH, getCustomizedResponseMessages());
 //    }
 //
-//    private List<ResponseMessage> getCustomizedResponseMessages() {
-//        List<ResponseMessage> responseMessages = new ArrayList<>();
-//        responseMessages.add(new ResponseMessageBuilder().code(200).message("성공").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(204).message("데이터 미존재").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(400).message("입력값 오류").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(401).message("미 로그인").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(403).message("권한없음").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(412).message("처리중 오류").build());
-//        responseMessages.add(new ResponseMessageBuilder().code(500).message("서버에러").build());
-//        return responseMessages;
-//    }
+    private List<ResponseMessage> getCustomizedResponseMessages() {
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder().code(200).message("성공").build());
+        responseMessages.add(new ResponseMessageBuilder().code(204).message("데이터 미존재").build());
+        responseMessages.add(new ResponseMessageBuilder().code(400).message("입력값 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(401).message("미 로그인").build());
+        responseMessages.add(new ResponseMessageBuilder().code(403).message("권한없음").build());
+        responseMessages.add(new ResponseMessageBuilder().code(412).message("처리중 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(500).message("서버에러").build());
+        return responseMessages;
+    }
 //
 //    //swagger ui 설정.
 //    @Bean
