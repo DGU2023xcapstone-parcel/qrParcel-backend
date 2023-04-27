c88b0d4992ba02bba5ab71e2324689f6d3ff543f
Kim jung won <k38836919@gmail.com>
2023-04-27 14:54:45 +0900

    feat : QRcode 도메인 작성

--- src/main/java/com/capstone/project/domain/entity/QRcode.java
+++ src/main/java/com/capstone/project/domain/entity/QRcode.java
@@ -1,2 +1,30 @@
-package com.capstone.project.domain.entity;public class QRcode {
+package com.capstone.project.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class QRcode {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "QRCODE_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String invoiceNo;
+
+    @Embedded
+    private Address receiverAddress;
+
+    @Column(nullable = false)
+    private String receiverName;
+
+    @Column(nullable = false)
+    private String receiverPhoneNumber;
+
 }
