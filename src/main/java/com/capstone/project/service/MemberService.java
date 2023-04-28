4e783a011e569f0dac7cd2b383e95d877c0f9323
Kim jung won <k38836919@gmail.com>
2023-04-28 13:50:14 +0900

    refact : 이메일로 유저 조회 및 비밀번호 판별 기능

--- src/main/java/com/capstone/project/service/MemberService.java
+++ src/main/java/com/capstone/project/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.capstone.project.service;
 
+import com.capstone.project.domain.constants.AuthConstants;
 import com.capstone.project.domain.dto.MemberDto;
 import com.capstone.project.domain.dto.SignUpDto;
 import com.capstone.project.domain.entity.Member;
+import com.capstone.project.domain.entity.TrackingInfo;
 import com.capstone.project.domain.enums.UserRole;
 import com.capstone.project.repository.MemberRepository;
 import com.capstone.project.utils.TokenUtils;
@@ -11,6 +13,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ public class MemberService {
     private final TokenUtils tokenUtils;
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private static final int PARSE_POS = 7;
 
     public Boolean isEmailDuplicated(final String email){
         return memberRepository.existsByEmail(email);
@@ -28,6 +33,10 @@ public class MemberService {
 
     public Member findByEmail(String email){
         return memberRepository.findByEmail(email).orElse(null);
+    }
+
+    public Member findByPhoneNumber(String phoneNumber){
+        return memberRepository.findByPhoneNumber(phoneNumber).orElse(null);
     }
 
     @Transactional
@@ -44,5 +53,59 @@ public class MemberService {
                 .name(signUpDto.getName())
                 .email(signUpDto.getEmail())
                 .build();
+    }
+
+    @Transactional
+    public MemberDto adminSignUp(final SignUpDto signUpDto){
+        memberRepository.save(
+                Member.builder()
+                        .name(signUpDto.getName())
+                        .email(signUpDto.getEmail())
+                        .password(bCryptPasswordEncoder.encode(signUpDto.getPassword()))
+                        .role(UserRole.ADMIN)
+                        .build());
+
+        return MemberDto.builder()
+                .name(signUpDto.getName())
+                .email(signUpDto.getEmail())
+                .build();
+    }
+
+    @Transactional
+    public MemberDto deliverySignUp(final SignUpDto signUpDto){
+        memberRepository.save(
+                Member.builder()
+                        .name(signUpDto.getName())
+                        .email(signUpDto.getEmail())
+                        .password(bCryptPasswordEncoder.encode(signUpDto.getPassword()))
+                        .role(UserRole.DELIVERY)
+                        .build());
+
+        return MemberDto.builder()
+                .name(signUpDto.getName())
+                .email(signUpDto.getEmail())
+                .build();
+    }
+
+    public void connectMemberToTracking(Member member, TrackingInfo trackingInfo){
+        member.getParcels().add(trackingInfo);
+    }
+
+    public Member getMemberThroughRequest(HttpServletRequest request) {
+        String author = request.getHeader(AuthConstants.AUTHORIZATION_HEADER);
+        String token = author.substring(PARSE_POS, author.length());
+        String email = tokenUtils.getUid(token);
+
+        return this.findByEmail(email);
+    }
+
+    public Boolean isMatchPassword(String email, String password){
+        Member member = memberRepository.findByEmail(email).orElse(null);
+        if(member == null) return false;
+        return bCryptPasswordEncoder.matches(member.getPassword(), password);
+    }
+
+    public Member getMember(String email){
+        return memberRepository.findByEmail(email).orElse(null);
     }
 }
